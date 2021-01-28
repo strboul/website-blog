3c547133a5a073f994cc6482eaf415adad7e9a80
Metin Yazici <stradivariusboul@gmail.com>
2021-01-28 23:28:36 +0100

    Typo fix in about/

--- pages/about.js
+++ pages/about.js
@@ -22,10 +22,10 @@ export const getStaticProps = async () => {
   I initially started blogging in June 2017 and been mainly writing about many
   different topics including, but not limited to, programming languages,
   statistics, data visualizations, databases and relational tables, web
-  applications and so on. If you find my writing interesting, follow me from
-  the links in the footer so we can be in touch. This site is made with the
+  applications, and so on. If you find my writing interesting, follow me from
+  the links in the footer so we can be in touch. This site is made with amazing
   [Next.js](https://nextjs.org/) and is currently hosted on
-  [Vercel](https://vercel.com/). Please note that some parts of the website is
+  [Vercel](https://vercel.com/). Please note that some parts of the website are
   still under construction 🚧.
   `;
   const mdxSource = await renderToString(source);
